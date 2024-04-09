cb3d9c2994e579d173ef149217e69e436dacb0c6
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-04-09 22:15:09 +0530

    Update wrapper.py

--- AdityaHalder/modules/helpers/wrapper.py
+++ AdityaHalder/modules/helpers/wrapper.py
@@ -68,7 +68,7 @@ def inline_wrapper(func):
                     [
                         InlineKeyboardButton(
                             "💥 Deploy Genius Userbot ✨",
-                            url=f"https://github.com/AdityaHalder/Genius-Userbot"
+                            url=f"https://github.com/Akash8t2/Ak-Genius-Userbot"
                         )
                     ]
                 ]
@@ -98,7 +98,7 @@ def inline_wrapper(func):
                             InlineQueryResultArticle(
                                 title="",
                                 input_message_content=InputTextMessageContent(
-                                    f"||**🥀 Please, Deploy Your Own Genius Userbot❗...\n\nRepo:** <i>https://github.com/AdityaHalder/Genius-Userbot/</i>||"
+                                    f"||**🥀 Please, Deploy Your Own Genius Userbot❗...\n\nRepo:** <i>https://github.com/Akash8t2/Ak-Genius-Userbot</i>||"
                                 ),
                             )
                         )
